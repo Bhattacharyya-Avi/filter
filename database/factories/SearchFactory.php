cf47893a3d56687d0f2df6d2660812d14ef235bb
Bhattacharyya-Avi <ab.kodeeo@gmail.com>
2022-10-12 11:28:10 +0600

    factories

---
+++ database/factories/SearchFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Search>
+ */
+class SearchFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id'=>$this->faker->numberBetween(1,10),
+            'keyword'=>$this->faker->word(),
+            'date'=>Carbon::now()->format('Y-m-d'),
+            'result'=> $this->faker->sentence(),
+
+        ];
+    }
+}
